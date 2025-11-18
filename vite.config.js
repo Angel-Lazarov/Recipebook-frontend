4e58e7fec76b40de25e45cc14220720545267739
acidlord <acidlord_34@abv.bg>
2025-11-18 18:09:06 +0200

    Remove need of local server on deploy.

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,19 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import path from "path";
 
+// Проверка дали сме локално
+const isLocal = process.env.LOCAL_HTTPS === "true"
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.join('certs', 'server.key')),
-      cert: fs.readFileSync(path.join('certs', 'server.cert')),
-    },
-    port: 5173,
-  },
+  server: isLocal
+    ? {
+      https: {
+        key: fs.readFileSync(path.join('certs', 'server.key')),
+        cert: fs.readFileSync(path.join('certs', 'server.cert')),
+      },
+      port: 5173,
+    }
+    : undefined, // за production / Render
 })
